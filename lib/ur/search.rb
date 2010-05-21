aea5be13db60e61915d59f87c1e1a0aed5e899fb
Peter Hellberg <peter@c7.se>
2010-05-21 15:15:20 +0200

    Catch the RSolr::RequestError

--- lib/ur/search.rb
+++ lib/ur/search.rb
@@ -12,35 +12,40 @@ module UR
     attr_reader :products, :solr, :facets
     
     def initialize(solr_params)
-      solr = RSolr.connect :url => SEARCH_SERVICE_URL
-      response = solr.find solr_params
-      
-      # Populate the products from the Metadata Cache
-      @products = (response.ok? && response.docs.size > 0) ?
-        Product.find(response.docs.map { |d| d.id }) : []
-      
-      # Expose the Solr response
-      @solr = response
-      
       # Prepare the facets
+      @products = []
       @facets = {}
-      @solr.facets.map { |f| @facets[f.name] = f.items } if @solr.facets.size > 0
-    end
-    
-    def num_found
-      @solr.response['numFound'].to_i
+      @solr   = nil
+      
+      begin 
+        solr = RSolr.connect :url => SEARCH_SERVICE_URL
+        response = solr.find solr_params
+        
+        # Populate the products from the Metadata Cache
+        @products = (response.ok? && response.docs.size > 0) ?
+          Product.find(response.docs.map { |d| d.id }) : []
+        
+        # Expose the Solr response
+        @solr = response
+        @solr.facets.map { |f| @facets[f.name] = f.items } if @solr.facets.size > 0
+      rescue RSolr::RequestError => e
+      end
     end
     
     def ok?
-      @solr.ok?
+      !@solr.nil? && @solr.ok?
+    end
+    
+    def num_found
+      (ok?) ? @solr.response['numFound'].to_i : 0
     end
     
     def previous_page
-      @solr.docs.previous_page
+      (ok? && !@solr.docs.nil?) ? @solr.docs.previous_page : 0
     end
     
     def next_page
-      @solr.docs.next_page
+      (ok? && !@solr.docs.nil?) ? @solr.docs.next_page : 0
     end
   end
 end
