26b7f11816683bba8f57876c7de75349aa3acf80
Peter Hellberg <peter@c7.se>
2010-06-18 13:00:59 +0200

    Fixed spelling mistake, added the method active?

--- lib/ur/product/distribution_event.rb
+++ lib/ur/product/distribution_event.rb
@@ -1,15 +1,19 @@
 module UR
   class Product
-    class DistributionEvent      
+    class DistributionEvent
       attr_reader :start_date, :end_date, :platform, 
                   :event_type, :receiving_agent_group
       
       def initialize(data)
         @start_date = Time.parse(data['startdate']) if !data['startdate'].nil?
         @end_date = Time.parse(data['enddate']) if !data['enddate'].nil?
-        @receiving_agent_group = data['recievingagentgroup']
+        @receiving_agent_group = data['receivingagentgroup']
         @platform = data['platform']
         @event_type = data['type']
+      end
+      
+      def active?
+        (start_date < Time.now && end_date > Time.now) ? true : false
       end
     end
   end
