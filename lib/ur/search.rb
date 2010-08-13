ed8ce3e511439722fbd83b6f933b51a2b59b4ee1
Peter Hellberg <peter@c7.se>
2010-08-13 16:47:25 +0200

    UR::Search.current_programs

--- lib/ur/search.rb
+++ lib/ur/search.rb
@@ -44,6 +44,28 @@ module UR
       (ok?) ? @solr.response['numFound'].to_i : 0
     end
     
+    def self.current_programs(per_page = 10, offset = 0, format = false, agerange = false)
+      url = "#{SEARCH_SERVICE_URL}/select?qt=current-products" +
+            "&rows=#{per_page}&start=#{offset}" +
+            "&publicstreaming=NOW" +
+            "&fq=(search_product_type:programtv" +
+            "%20OR%20search_product_type:programradio)"
+
+      if ['audio','video'].include? format
+        url += "%20AND%20format:#{format}"
+      end
+
+      url += agerange_filter(agerange)
+
+      programs = Yajl::HttpStream.get(url)
+      
+      if (programs['response']['docs'].count > 0) 
+        Product.find(programs['response']['docs'].map { |d| d['id'] })
+      else
+        []
+      end
+    end
+    
     ##
     # Pagination
     #
@@ -62,6 +84,28 @@ module UR
     
     def next_page
       (current_page < total_pages) ? @solr.docs.next_page : false
+    end
+    
+  private
+    def self.agerange_filter(agerange, prepend = '%20AND%20')
+      if ['children', 'youths', 'adults'].include? agerange
+        filter = '(typicalagerange:' + 
+        { 
+          'children' => ['preschool', 'primary0-3', 'primary4-6'],
+          'youths'   => ['primary7-9', 'secondary'],
+          'adults'   => ['schoolvux', 'komvuxgrundvux', 'teachereducation',
+                         'popularadulteducation', 'folkhighschool', 'university']
+        }[agerange].join('%20OR%20typicalagerange:') + ')'
+
+        if prepend
+          filter = prepend + filter
+        end
+
+      else
+        filter = ''
+      end
+
+      filter
     end
   end
 end
--- ur-product.gemspec
+++ ur-product.gemspec
@@ -1,7 +1,7 @@
 Gem::Specification.new do |s|
   s.name          = "ur-product"
-  s.version       = "0.9.8"
-  s.date          = "2010-06-30"
+  s.version       = "0.9.9"
+  s.date          = "2010-08-13"
   s.summary       = "API wrapper for the Utbildningsradion product services"
   s.description   = "Enables searching and fetching of Utbildningsradion products"
   s.has_rdoc      = false
