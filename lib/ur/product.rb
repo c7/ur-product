ed3ef3b779481c30a41d5ad94a515cd054d9edb8
Peter Hellberg <peter@c7.se>
2011-01-07 12:11:44 +0100

    Whitespace cleanup

--- lib/ur/product.rb
+++ lib/ur/product.rb
@@ -4,36 +4,36 @@ require 'restclient'
 # Module for Utbildningsradion AB (http://ur.se/)
 module UR
   # A product can be a tv show, a radio program or even a website
-  class Product    
+  class Product
     # Setup
     if !defined?(ASSETS_URL)
       ASSETS_URL = 'http://assets.ur.se'
     end
-    
+
     attr_reader :related_products,
-                :ur_product_id, :title, :languages, 
+                :ur_product_id, :title, :languages,
                 :description, :easy_to_read_description,
                 :obsolete_order_id, :status, :production_year,
                 :main_title, :remainder_of_title, :producing_company,
                 :created_at, :modified_at, :format, :duration, :aspect_ratio,
-                :product_type, :product_sub_type, :typical_age_ranges, 
-                :publication_date, :storages, :distribution_events, :difficulty, 
+                :product_type, :product_sub_type, :typical_age_ranges,
+                :publication_date, :storages, :distribution_events, :difficulty,
                 :sli, :sli_sub, :sab, :sao, :fao, :related_product_ids
-    
+
     def initialize(data)
       unless !data['status'].nil? && data['status'] == 404
         product_data = data.include?('product') ? data['product'] : data
         relations = data.include?('relations') ? data['relations'] : []
         populate(product_data, relations)
-        
+
         self.class.define_boolean_methods([
           'distribution_events', 'storages', 'typical_age_ranges', 'languages',
-          'duration', 'difficulty', 'producing_company', 'production_year', 
+          'duration', 'difficulty', 'producing_company', 'production_year',
           'obsolete_order_id', 'sli', 'sli_sub', 'sab', 'sao', 'fao'
         ])
         self.class.define_relation_boolean_methods([
-          'siblings', 'packageseries', 'packageusageseries', 'website', 
-          'packagedvd', 'packagecd', 'programtv', 'programradio', 
+          'siblings', 'packageseries', 'packageusageseries', 'website',
+          'packagedvd', 'packagecd', 'programtv', 'programradio',
           'trailertrailer'
         ])
         def ok?; true; end
@@ -41,61 +41,61 @@ module UR
         def ok?; false; end
       end
     end
-    
+
     def self.find(id)
       data = MetadataCache.find(id)
-      
-      (data.instance_of?(Array)) ? 
+
+      (data.instance_of?(Array)) ?
         data.map { |d| Product.new(d) } : Product.new(data)
     end
-    
+
     def self.search(solr_params)
       UR::Search.new(solr_params)
     end
-    
+
     def self.define_boolean_methods(names)
       names.each do |name|
         define_method("has_#{name}?") do
-          instance_variable = instance_variable_get("@#{name}")            
+          instance_variable = instance_variable_get("@#{name}")
           !(instance_variable.nil? || instance_variable.to_s.empty?)
         end
       end
     end
-    
+
     def self.define_relation_boolean_methods(names)
       names.each do |name|
         define_method("has_#{name}?") do
           related_products = instance_variable_get("@related_products")
-          (!related_products.nil? && 
-            (!related_products[name].nil? && 
+          (!related_products.nil? &&
+            (!related_products[name].nil? &&
               !related_products[name].to_s.empty?))
         end
       end
     end
-    
+
     def self.define_relation_accessor(name)
       define_method(name) do
         instance_variable_get("@related_products")[name]
       end
     end
-    
+
     # Populate the object
     def populate(product_data, relations)
       # Handle the fields
       standard_fields = [
-        'id', 'obsoleteorderid', 'status', 'language', 
+        'id', 'obsoleteorderid', 'status', 'language',
         'title.sv', 'maintitle.sv', 'remainderoftitle.sv',
         'description.sv', 'easytoreaddescription.sv',
         'productionyear', 'producingcompany',
-        'created', 'modified', 'pubdate', 
+        'created', 'modified', 'pubdate',
         'format', 'duration', 'aspect_ratio',
-        'product_type', 'product_sub_type', 
-        'typicalagerange', 'difficulty', 
+        'product_type', 'product_sub_type',
+        'typicalagerange', 'difficulty',
         'sli', 'sli_sub', 'sab', 'sao', 'fao'
       ]
-      
+
       field_names = lambda do |name|
-        renamed = { 
+        renamed = {
           'id' => 'ur_product_id',
           'maintitle' => 'main_title',
           'remainderoftitle' => 'remainder_of_title',
@@ -111,23 +111,23 @@ module UR
         }
         (renamed.keys.include?(name)) ? renamed[name] : name
       end
-      
+
       standard_fields.each do |field|
         field,lang = field.split('.')
-        
+
         if lang.nil?
           value = product_data[field]
         elsif !product_data[field].nil?
           value = product_data[field][lang]
         end
-        
+
         if ['pubdate', 'created', 'modified'].include? field
           value = Time.parse(value)
         end
-        
+
         instance_variable_set("@#{field_names.call(field)}", value)
       end
-      
+
       # Handle the data structures
       [
         ['distributionevent', 'distribution_events', UR::Product::DistributionEvent],
@@ -135,74 +135,74 @@ module UR
         ['relation_haspart', 'related_product_ids', UR::Product::RelatedProductId],
       ].each do |name, variable, structure_class|
         data = product_data[name]
-        instance_variable_set("@#{variable}", (!data.nil? && data.size > 0) ? 
+        instance_variable_set("@#{variable}", (!data.nil? && data.size > 0) ?
           data.map { |d| structure_class.new d } : [])
       end
-      
+
       # Handle the relations
       @related_products = {}
       if relations.size > 0
         @has_relations = true
         relations.each do |name, products|
           @related_products[name] = []
-          
+
           products.each do |related_product_data|
             @related_products[name] << Product.new(related_product_data)
           end
-          
+
           self.class.define_relation_accessor(name)
         end
       end
     end
-    
+
     def available_on_avc?
       return @available_on_avc unless @available_on_avc.nil?
       @available_on_avc = false
-      
+
       distribution_events.each do |event|
         if event.receiving_agent_group == 'avc'
           @available_on_avc = event.active?
           break
         end
       end
-      
+
       @available_on_avc
     end
-    
+
     def broadcasts
       return @broadcasts unless @broadcasts.nil?
       @broadcasts = []
-      
+
       distribution_events.each do |event|
         @broadcasts << event if event.event_type == 'broadcast'
       end
-      
+
       @broadcasts = @broadcasts.sort_by { |broadcast| broadcast.start_date }
       @broadcasts
     end
-    
+
     def documents
       return @docs unless @docs.nil?
       @docs = []
-      
+
       [
-        'textteacherguide', 
-        'textstudyguide', 
-        'textscript', 
+        'textteacherguide',
+        'textstudyguide',
+        'textscript',
         'textworksheet',
-        'texttasks', 
+        'texttasks',
         'texttext'
       ].each do |name|
         @docs << @related_products[name] if !@related_products[name].nil?
       end
-      
+
       @docs.flatten!
     end
-    
+
     def has_documents?
       (!documents.nil? && documents.size > 0)
     end
-    
+
     def humanized_duration
       if matched = duration.match(/^(\d\d):(\d\d):(\d\d)/)
         (full,h,m,s) = matched.to_a
@@ -222,46 +222,46 @@ module UR
       broadcast_format = (product_type == 'package') ? "-#{format}" : ''
       (product_sub_type.nil?) ? product_type : "#{product_type}#{product_sub_type}#{broadcast_format}"
     end
-    
+
     def image_url(number = 1, size = '')
       "#{ASSETS_URL}/id/#{ur_product_id}/images/#{number}#{size}.jpg"
     end
-  
+
     def has_image?(number = 1, size = '')
       return @has_image if !@has_image.nil?
-      
+
       begin
         url = "#{ASSETS_URL}/id/#{ur_product_id}/images/#{number}#{size}.jpg"
         @has_image = (RestClient.head(url).headers[:x_ur_http_status] == "200")
       rescue RestClient::ResourceNotFound
         @has_image = false
       end
-      
+
       @has_image
     end
 
     def has_relations?
       (!@related_products.nil? && @related_products.size > 0)
     end
-    
+
     def get_storage_location(format)
       storage = storages.map { |s| s if s.storage_format == format }.compact
       storage.empty? ? false : storage.first.location
     end
-    
+
     def media_id
-      @media_id ||= (get_storage_location('genericvideo') || 
+      @media_id ||= (get_storage_location('genericvideo') ||
                      get_storage_location('genericaudio'))
       @media_id
     end
-    
+
     def has_media_id?
       (media_id)
     end
-    
+
     def url
       return @url unless @url.nil?
-      
+
       if product_type == 'website'
         @url = get_storage_location('url')
       elsif respond_to?(:website) && website.respond_to?(:first)
@@ -276,11 +276,11 @@ module UR
 
       @url
     end
-    
+
     def has_url?
       (url)
     end
-    
+
     def short_title
       if !remainder_of_title.nil?
         remainder_of_title
